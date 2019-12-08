ad3bdf0535275423a1b9475f259d13d0481e39da
Ilya <isachek2001@gmail.com>
2019-12-08 13:08:05 +0300

    task 6 is finished, but need some little modfications

--- 6 лаба/6.cpp
+++ 6 лаба/6.cpp
@@ -5,8 +5,8 @@
 float Norm(const std::vector<float>& vec) {
 	float max = 0;
 	for (const auto& el : vec) {
-		if (max < el) {
-			max = el;
+		if (max < abs(el)) {
+			max = abs(el);
 		}
 	}
 	return max;
@@ -50,6 +50,19 @@ std::vector<float> Eigenvectors(const std::vector<float>& u, const std::vector<f
 	return eigenvector;
 }
 
+float CountSecondEigenValue(std::vector<float> v_m, std::vector<float> v_m1, 
+								std::vector<float> u_m, std::vector<float> u_m1, float lambda) {
+	float max = 0;
+	int index = 0;
+	for(int i = 0; i < v_m.size(); i++) {
+		if (abs(v_m[i] - lambda * u_m1[i]) > max) {
+			max = abs(v_m[i] - lambda * u_m1[i]);
+			index= i;
+		}
+	}
+	return (v_m1[index] * Norm(v_m) - lambda * v_m[index]) / (v_m[index] - lambda * u_m1[index]);
+}
+
 class Matrix {
 public:
 
@@ -61,7 +74,7 @@ public:
 	Matrix() {
 		srand(time(NULL));
 		// случайное количество элементов матрицы
-		int n = rand() % 3 + 10;
+		int n = 4;//rand() % 3 + 10;
 		// запоминаем числа для не диагональных элементов от -4 до 0
 		std::vector<int> no_diag;
 		for (int i = 0; i < (n * n - n) / 2; i++) {
@@ -100,50 +113,82 @@ public:
 		for (int i = 1; i < matrix_.size(); i++) {
 			matrix_[i][i] = sum_1_col - matrix_[i][0];
 		}
-
-		int m = 17;
-		for (int i = 0; i < n; i++) {
-			x_.push_back(m + i);
-		}
-		// multiply x_ on matrix to get vector b
-		// if after Gauss method answer vector and x_ will be the same
-		// then we will know method works correctly
-		b_ = MultMat(matrix_, x_);
-
+		matrix_ = { { -23, -18, -35, 28 },
+					{ 5, 10, 5, -9 },
+					{ -49, 12, 25, 16 },
+					{ 26, -48, -42, 21} };
 		// write matrices in the logs.txt
 		LogMatrix(matrix_);
 	}
 
 	void StartIterations() {
 		int line_length = matrix_.size();
-		std::vector<float> x_1(line_length), x_2(line_length);
+		std::vector<float> x_1(line_length), x_2(line_length),
+			v_30, v_31, u_30, u_29, v_49, v_50, u_49, u_48;
 		x_2[0] = 1;
 		float max = 0;
 		for (int i = 0; i < 46; i++) {
+			if (i == 30) {
+				v_30 = x_2;
+			}
+			if (i == 31) {
+				v_31 = x_2;
+			}
 			Normalization(x_2);
+			if (i == 29) {
+				u_29 = x_2;
+			}
+			if (i == 30) {
+				u_30 = x_2;
+			}
+			std::cout << "k = " << i << ":   ";
+			PrintVector(x_2);
 			x_2 = MultMat(matrix_, x_2);
 			max = 0;
 		}
+		std::cout << std::endl;
 		float lamda_simple = 0, lamda_scalar_product = 0;
 		for (int i = 46; i < 51; i++) {
+			if (i == 49) {
+				v_49 = x_2;
+			}
+			if (i == 50) {
+				v_50 = x_2;
+			}
 			x_1 = x_2;
 			Normalization(x_1);
+			if (i == 49) {
+				u_49 = x_1;
+			}
+			if (i == 48) {
+				u_48 = x_1;
+			}
+			std::cout << "k = " << i << ":   ";
 			PrintVector(x_1);
 			x_2 = MultMat(matrix_, x_1);
-			float lamda_simple = Norm(x_2) * Sign(Norm(x_1));
-			float lamda_scalar_product = ScalarProduct(x_2, x_1) / ScalarProduct(x_1, x_1);
+			lamda_simple = Norm(x_2) * Sign(Norm(x_1));
+			lamda_scalar_product = ScalarProduct(x_2, x_1) / ScalarProduct(x_1, x_1);
 		}
 		std::vector<float> eigenvector_simple = Eigenvectors(x_1, x_2, lamda_simple);
 		std::vector<float> eigenvector_scalar = Eigenvectors(x_1, x_2, lamda_scalar_product);
-		Norm(eigenvector_simple);
-		Norm(eigenvector_scalar);
+		PrintVector(eigenvector_simple);
+		std::cout << std::endl << std::endl;
+		PrintVector(eigenvector_simple);
+		std::cout << std::endl << std::endl;
+		std::cout << "Eigenvalue by lambda = v / u: " << lamda_simple << std::endl
+				<< "Eigenvalue by lambda <v, u> / <u, u>: " << lamda_scalar_product;
+
+		float eigenvalue_30 = CountSecondEigenValue(v_30, v_31, u_30, u_29, lamda_simple);
+		float eigenvalue_50 = CountSecondEigenValue(v_49, v_50, u_49, u_48, lamda_simple);
+		float eigenvalue_50_scalar_eigenval = CountSecondEigenValue(v_49, v_50, u_49, u_48, lamda_scalar_product);
+		std::cout << std::endl << std::endl << eigenvalue_30 << "  " << eigenvalue_50 << "  " << eigenvalue_50_scalar_eigenval << std::endl;
+
+
 	}
 
 private:
 	std::vector<std::vector<float>> matrix_;
-	std::vector<float> f_, x_, b_, x_curr_;
-	int k_max_, k_;
-	float E_;
+	std::vector<float> x_;
 };
 
 int main() {
