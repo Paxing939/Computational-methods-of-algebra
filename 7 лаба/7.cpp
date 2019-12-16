966b08691a6653f8df98f60bc918c6bdad2f70c9
Ilya <isachek2001@gmail.com>
2019-12-16 11:34:18 +0300

    Newton method at task 7 works

--- 7 лаба/7.cpp
+++ 7 лаба/7.cpp
@@ -72,9 +72,9 @@ float XFromPolynom(std::vector<float> coeffs, float x) {
 }
 
 float XFromPolynom2(std::vector<float> coeffs, float x) {
-	//return -(coeffs[3] * x * x * x + coeffs[2] * x * x + coeffs[0]) / coeffs[1];
-	return -pow((coeffs[2] * x * x + coeffs[1] * x + coeffs[0]) / coeffs[3], 1 / 3);
-	return (coeffs[2] + coeffs[1] / x + coeffs[0] / (x * x)) / coeffs[3];
+	return -(coeffs[3] * x * x * x + coeffs[2] * x * x + coeffs[0]) / coeffs[1];
+	//return -pow((coeffs[2] * x * x + coeffs[1] * x + coeffs[0]) / coeffs[3], 1 / 3);
+	//return (coeffs[2] + coeffs[1] / x + coeffs[0] / (x * x)) / coeffs[3];
 }
 
 float XFromPolynom22(std::vector<float> coeffs, float x) {
@@ -95,12 +95,168 @@ public:
 	}
 
 	Polinomial() {
-		coeffs_ = { -5, -20, -4, 4, 1 };
+		coeffs_ = { 179000, 56300, -659, -100, 1 };
 		accurancy_ = 0.0000001;
 	}
 
 	void StartIterations() {
-		std::vector<float> derivative_3 = DecreaseCoeffs(FindDerivative(coeffs_));
+		//std::vector<float> derivative_3 = FindDerivative(coeffs_);
+		//std::vector<float> derivative_2 = FindDerivative(derivative_3);
+		//std::vector<float> derivative_2_answers = Solve2Equation(derivative_2), derivative_3_answers, answers;
+		//if (derivative_2_answers[0] > derivative_2_answers[1]) {
+		//	std::swap(derivative_2_answers[0], derivative_2_answers[1]);
+		//}
+
+		////#define DEBAG 1
+		////		derivative_3 = { 5, -8, -8, 5 };
+		////		derivative_2_answers = { -0.37098, 1.4376 };
+
+		//float margin = 5;
+		//float x_2 = derivative_2_answers[0], x_1 = derivative_2_answers[0] - margin;
+		//float y_1 = CountPolynomFromX(derivative_3, x_1), y_2 = CountPolynomFromX(derivative_3, x_2);
+		//for (int i = 0; i < 100; i++) {
+		//	if (OneSign(y_1, y_2)) {
+		//		x_2--;
+		//		y_2 = CountPolynomFromX(derivative_3, x_2);
+		//	}
+		//	else {
+		//		break;
+		//	}
+		//}
+		//float x = (x_1 + x_2) / 2, x_next = 0;
+		//for (int i = 0; i < 1500; i++) {
+		//	x_next = XFromPolynom2(derivative_3, x);
+		//	if (abs(x_next - x) < accurancy_) {
+		//		break;
+		//	}
+		//	x = x_next;
+		//}
+		//std::cout << x << std::endl;
+		//derivative_3_answers.push_back(x);
+		//std::cout << x << std::endl;
+
+		//x_1 = derivative_2_answers[0], x_2 = derivative_2_answers[1];
+		//y_1 = CountPolynomFromX(derivative_3, x_1), y_2 = CountPolynomFromX(derivative_3, x_2);
+		//if (OneSign(y_1, y_2)) {
+		//	std::cout << "Complex root in f'(x)!" << std::endl;
+		//	exit(0);
+		//}
+		////for (int i = 0; i < 100; i++) {
+		////	if (!OneSign(CountPolynomFromX(derivative_3, x_1 + 1), y_2)) {
+		////		x_1++;
+		////		y_1 = CountPolynomFromX(derivative_3, x_1);
+		////	} else {
+		////		if (!OneSign(CountPolynomFromX(derivative_3, x_2 - 1), y_1)) {
+		////			x_2--;
+		////			y_1 = CountPolynomFromX(derivative_3, x_2);
+		////		} else {
+		////			break;
+		////		}
+		////	}
+		////}
+		//x = (x_2 - x_1) / 2, x_next = 0;
+		//for (int i = 0; i < 500; i++) {
+		//	x_next = XFromPolynom2(derivative_3, x);
+		//	if (abs(x_next - x) < accurancy_) {
+		//		break;
+		//	}
+		//	x = x_next;
+		//}
+		//derivative_3_answers.push_back(x);
+		//std::cout << x << std::endl;
+
+
+		//x_1 = derivative_2_answers[1], x_2 = derivative_2_answers[1] + 10;
+		//y_1 = CountPolynomFromX(derivative_3, x_1), y_2 = CountPolynomFromX(derivative_3, x_2);
+		//for (int i = 0; i < 100; i++) {
+		//	if (OneSign(y_1, y_2)) {
+		//		x_2++;
+		//		y_2 = CountPolynomFromX(derivative_3, x_2);
+		//	}
+		//}
+		//x = x_1, x_next = 0;
+		//for (int i = 0; i < 500; i++) {
+		//	x_next = XFromPolynom2(derivative_3, x);
+		//	if (abs(x_next - x) < accurancy_) {
+		//		break;
+		//	}
+		//	x = x_next;
+		//}
+		//derivative_3_answers.push_back(x);
+		//std::cout << x << std::endl << std::endl;
+
+
+
+
+		//x_2 = derivative_3_answers[0], x_1 = derivative_3_answers[0] - margin;
+		//y_1 = CountPolynomFromX(coeffs_, x_1), y_2 = CountPolynomFromX(coeffs_, x_2);
+		//for (int i = 0; i < 100; i++) {
+		//	if (OneSign(y_1, y_2)) {
+		//		x_1--;
+		//	}
+		//	y_1 = CountPolynomFromX(coeffs_, x_1);
+		//}
+		//x = (x_1 - x_2) / 2, x_next = 0;
+		//for (int i = 0; i < 1500; i++) {
+		//	x_next = XFromPolynom22(coeffs_, x);
+		//	if (abs(x_next - x) < accurancy_) {
+		//		break;
+		//	}
+		//	x = x_next;
+		//}
+		//answers.push_back(x);
+		//std::cout << x << std::endl;
+
+		//x_2 = derivative_3_answers[0], x_1 = derivative_3_answers[1];
+		//y_1 = CountPolynomFromX(coeffs_, x_1), y_2 = CountPolynomFromX(coeffs_, x_2);
+		//x = (x_1 - x_2) / 2, x_next = 0;
+		//for (int i = 0; i < 1500; i++) {
+		//	x_next = XFromPolynom22(coeffs_, x);
+		//	if (abs(x_next - x) < accurancy_) {
+		//		break;
+		//	}
+		//	x = x_next;
+		//}
+		//answers.push_back(x);
+		//std::cout << x << std::endl;
+
+		//x_2 = derivative_3_answers[1], x_1 = derivative_3_answers[2];
+		//y_1 = CountPolynomFromX(coeffs_, x_1), y_2 = CountPolynomFromX(coeffs_, x_2);
+		//x = (x_1 - x_2) / 2, x_next = 0;
+		//for (int i = 0; i < 1500; i++) {
+		//	x_next = XFromPolynom22(coeffs_, x);
+		//	if (abs(x_next - x) < accurancy_) {
+		//		break;
+		//	}
+		//	x = x_next;
+		//}
+		//std::cout << x << std::endl;
+		//answers.push_back(x);
+		//std::cout << x << std::endl;
+
+		//x_2 = derivative_3_answers[2], x_1 = derivative_3_answers[2] + margin;
+		//y_1 = CountPolynomFromX(coeffs_, x_1), y_2 = CountPolynomFromX(coeffs_, x_2);
+		//for (int i = 0; i < 100; i++) {
+		//	if (OneSign(y_1, y_2)) {
+		//		x_1++;
+		//	}
+		//	y_1 = CountPolynomFromX(coeffs_, x_1);
+		//}
+		//x = (x_1 - x_2) / 2, x_next = 0;
+		//for (int i = 0; i < 1500; i++) {
+		//	x_next = XFromPolynom22(coeffs_, x);
+		//	if (abs(x_next - x) < accurancy_) {
+		//		break;
+		//	}
+		//	x = x_next;
+		//}
+		//std::cout << x << std::endl;
+		//answers.push_back(x);
+		//std::cout << x << std::endl << std::endl << std::endl << std::endl;
+
+
+
+		std::vector<float> derivative_3 = /*DecreaseCoeffs*/(FindDerivative(coeffs_));
 		std::vector<float> derivative_2 = FindDerivative(derivative_3);
 		std::vector<float> derivative_2_answers = Solve2Equation(derivative_2), derivative_3_answers, answers;
 		if (derivative_2_answers[0] > derivative_2_answers[1]) {
@@ -116,8 +272,8 @@ public:
 		float y_1 = CountPolynomFromX(derivative_3, x_1), y_2 = CountPolynomFromX(derivative_3, x_2);
 		for (int i = 0; i < 100; i++) {
 			if (OneSign(y_1, y_2)) {
-				x_2--;
-				y_2 = CountPolynomFromX(derivative_3, x_2);
+				x_1--;
+				y_1 = CountPolynomFromX(derivative_3, x_1);
 			}
 			else {
 				break;
@@ -133,7 +289,6 @@ public:
 		}
 		std::cout << x << std::endl;
 		derivative_3_answers.push_back(x);
-		std::cout << x << std::endl;
 
 		x_1 = derivative_2_answers[0], x_2 = derivative_2_answers[1];
 		y_1 = CountPolynomFromX(derivative_3, x_1), y_2 = CountPolynomFromX(derivative_3, x_2);
@@ -154,7 +309,7 @@ public:
 		//		}
 		//	}
 		//}
-		x = (x_2 - x_1) / 2, x_next = 0;
+		x = (x_1 + x_2) / 2, x_next = 0;
 		for (int i = 0; i < 500; i++) {
 			x_next = XFromPolynom2(derivative_3, x);
 			if (abs(x_next - x) < accurancy_) {
@@ -174,7 +329,7 @@ public:
 				y_2 = CountPolynomFromX(derivative_3, x_2);
 			}
 		}
-		x = x_1, x_next = 0;
+		x = (x_1 + x_2) / 2, x_next = 0;
 		for (int i = 0; i < 500; i++) {
 			x_next = XFromPolynom2(derivative_3, x);
 			if (abs(x_next - x) < accurancy_) {
@@ -193,12 +348,14 @@ public:
 		for (int i = 0; i < 100; i++) {
 			if (OneSign(y_1, y_2)) {
 				x_1--;
+				y_1 = CountPolynomFromX(coeffs_, x_1);
 			}
-			y_1 = CountPolynomFromX(coeffs_, x_1);
 		}
-		x = (x_1 - x_2) / 2, x_next = 0;
+		x = (x_1 + x_2) / 2, x_next = 0;
 		for (int i = 0; i < 1500; i++) {
-			x_next = XFromPolynom22(coeffs_, x);
+			float a = CountPolynomFromX(coeffs_, x);
+			float b = CountPolynomFromX(derivative_3, x);
+			x_next = x - a / b;
 			if (abs(x_next - x) < accurancy_) {
 				break;
 			}
@@ -209,22 +366,22 @@ public:
 
 		x_2 = derivative_3_answers[0], x_1 = derivative_3_answers[1];
 		y_1 = CountPolynomFromX(coeffs_, x_1), y_2 = CountPolynomFromX(coeffs_, x_2);
-		x = (x_1 - x_2) / 2, x_next = 0;
+		x = (x_1 + x_2) / 2, x_next = 0;
 		for (int i = 0; i < 1500; i++) {
-			x_next = XFromPolynom22(coeffs_, x);
+			x_next = x - CountPolynomFromX(coeffs_, x) / CountPolynomFromX(derivative_3, x);
 			if (abs(x_next - x) < accurancy_) {
 				break;
 			}
 			x = x_next;
 		}
-		answers.push_back(x);
 		std::cout << x << std::endl;
+		answers.push_back(x);
 
 		x_2 = derivative_3_answers[1], x_1 = derivative_3_answers[2];
 		y_1 = CountPolynomFromX(coeffs_, x_1), y_2 = CountPolynomFromX(coeffs_, x_2);
-		x = (x_1 - x_2) / 2, x_next = 0;
+		x = (x_1 + x_2) / 2, x_next = 0;
 		for (int i = 0; i < 1500; i++) {
-			x_next = XFromPolynom22(coeffs_, x);
+			x_next = x - CountPolynomFromX(coeffs_, x) / CountPolynomFromX(derivative_3, x);
 			if (abs(x_next - x) < accurancy_) {
 				break;
 			}
@@ -232,19 +389,18 @@ public:
 		}
 		std::cout << x << std::endl;
 		answers.push_back(x);
-		std::cout << x << std::endl;
 
 		x_2 = derivative_3_answers[2], x_1 = derivative_3_answers[2] + margin;
 		y_1 = CountPolynomFromX(coeffs_, x_1), y_2 = CountPolynomFromX(coeffs_, x_2);
 		for (int i = 0; i < 100; i++) {
 			if (OneSign(y_1, y_2)) {
-				x_1++;
+				x_2++;
+				y_2 = CountPolynomFromX(coeffs_, x_2);
 			}
-			y_1 = CountPolynomFromX(coeffs_, x_1);
 		}
-		x = (x_1 - x_2) / 2, x_next = 0;
+		x = x_1, x_next = 0;
 		for (int i = 0; i < 1500; i++) {
-			x_next = XFromPolynom22(coeffs_, x);
+			x_next = x - CountPolynomFromX(coeffs_, x) / CountPolynomFromX(derivative_3, x);
 			if (abs(x_next - x) < accurancy_) {
 				break;
 			}
@@ -252,7 +408,6 @@ public:
 		}
 		std::cout << x << std::endl;
 		answers.push_back(x);
-		std::cout << x << std::endl << std::endl << std::endl << std::endl;
 	}
 
 	void Newton() {
@@ -266,7 +421,7 @@ public:
 		//}
 
 
-		std::vector<float> derivative_3 = DecreaseCoeffs(FindDerivative(coeffs_));
+		std::vector<float> derivative_3 = /*DecreaseCoeffs*/(FindDerivative(coeffs_));
 		std::vector<float> derivative_2 = FindDerivative(derivative_3);
 		std::vector<float> derivative_2_answers = Solve2Equation(derivative_2), derivative_3_answers, answers;
 		if (derivative_2_answers[0] > derivative_2_answers[1]) {
@@ -282,16 +437,16 @@ public:
 		float y_1 = CountPolynomFromX(derivative_3, x_1), y_2 = CountPolynomFromX(derivative_3, x_2);
 		for (int i = 0; i < 100; i++) {
 			if (OneSign(y_1, y_2)) {
-				x_2--;
-				y_2 = CountPolynomFromX(derivative_3, x_2);
+				x_1--;
+				y_1 = CountPolynomFromX(derivative_3, x_1);
 			}
 			else {
 				break;
 			}
 		}
-		float x = x_1, x_next = 0;
+		float x = (x_1 + x_2) /2, x_next = 0;
 		for (int i = 0; i < 1500; i++) {
-			x_next = x - CountPolynomFromX(coeffs_, x) / CountPolynomFromX(derivative_3, x);
+			x_next = x - CountPolynomFromX(derivative_3, x) / CountPolynomFromX(derivative_2, x);
 			if (abs(x_next - x) < accurancy_) {
 				break;
 			}
@@ -319,9 +474,9 @@ public:
 		//		}
 		//	}
 		//}
-		x = x_1, x_next = 0;
+		x = (x_1 + x_2)/2, x_next = 0;
 		for (int i = 0; i < 500; i++) {
-			x_next = x - CountPolynomFromX(coeffs_, x) / CountPolynomFromX(derivative_3, x);
+			x_next = x - CountPolynomFromX(derivative_3, x) / CountPolynomFromX(derivative_2, x);
 			if (abs(x_next - x) < accurancy_) {
 				break;
 			}
@@ -339,9 +494,11 @@ public:
 				y_2 = CountPolynomFromX(derivative_3, x_2);
 			}
 		}
-		x = x_1, x_next = 0;
+		x = (x_1 + x_2) / 2, x_next = 0;
 		for (int i = 0; i < 500; i++) {
-			x_next = x - CountPolynomFromX(coeffs_, x) / CountPolynomFromX(derivative_3, x);
+			float a = CountPolynomFromX(derivative_3, x);
+			float b = CountPolynomFromX(derivative_2, x);
+				x_next = x - a / b;
 			if (abs(x_next - x) < accurancy_) {
 				break;
 			}
@@ -358,12 +515,14 @@ public:
 		for (int i = 0; i < 100; i++) {
 			if (OneSign(y_1, y_2)) {
 				x_1--;
+				y_1 = CountPolynomFromX(coeffs_, x_1);
 			}
-			y_1 = CountPolynomFromX(coeffs_, x_1);
 		}
-		x = (x_1 - x_2) / 2, x_next = 0;
+		x = (x_1 + x_2) / 2, x_next = 0;
 		for (int i = 0; i < 1500; i++) {
-			x_next = x - CountPolynomFromX(coeffs_, x) / CountPolynomFromX(derivative_3, x);
+			float a = CountPolynomFromX(coeffs_, x);
+			float b = CountPolynomFromX(derivative_3, x);
+			x_next = x - a / b;
 			if (abs(x_next - x) < accurancy_) {
 				break;
 			}
@@ -374,7 +533,7 @@ public:
 
 		x_2 = derivative_3_answers[0], x_1 = derivative_3_answers[1];
 		y_1 = CountPolynomFromX(coeffs_, x_1), y_2 = CountPolynomFromX(coeffs_, x_2);
-		x = x_1, x_next = 0;
+		x = (x_1 + x_2) / 2, x_next = 0;
 		for (int i = 0; i < 1500; i++) {
 			x_next = x - CountPolynomFromX(coeffs_, x) / CountPolynomFromX(derivative_3, x);
 			if (abs(x_next - x) < accurancy_) {
@@ -387,7 +546,7 @@ public:
 
 		x_2 = derivative_3_answers[1], x_1 = derivative_3_answers[2];
 		y_1 = CountPolynomFromX(coeffs_, x_1), y_2 = CountPolynomFromX(coeffs_, x_2);
-		x = x_1, x_next = 0;
+		x = (x_1 + x_2) / 2, x_next = 0;
 		for (int i = 0; i < 1500; i++) {
 			x_next = x - CountPolynomFromX(coeffs_, x) / CountPolynomFromX(derivative_3, x);
 			if (abs(x_next - x) < accurancy_) {
@@ -402,9 +561,9 @@ public:
 		y_1 = CountPolynomFromX(coeffs_, x_1), y_2 = CountPolynomFromX(coeffs_, x_2);
 		for (int i = 0; i < 100; i++) {
 			if (OneSign(y_1, y_2)) {
-				x_1++;
+				x_2++;
+				y_2 = CountPolynomFromX(coeffs_, x_2);
 			}
-			y_1 = CountPolynomFromX(coeffs_, x_1);
 		}
 		x = x_1, x_next = 0;
 		for (int i = 0; i < 1500; i++) {
